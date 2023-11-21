7e8cd8bf0b65d0371dd37922231a4170a0d8c407
CurtLH <CurtLHampton@gmail.com>
2023-11-21 12:32:38 -0500

    Add black

--- funky_dingo/__init__.py
+++ funky_dingo/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for funky_dingo."""
 
 __author__ = """Curtis Hampton"""
-__email__ = 'curtlh@gmail.com'
-__version__ = '0.1.0'
+__email__ = "curtlh@gmail.com"
+__version__ = "0.1.0"
